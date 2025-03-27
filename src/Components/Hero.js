431df40640f04aa47e07e669d7389ed8439da4f4
bg14891 <bayronfranciscogodoy@gmail.com>
2025-03-26 20:45:59 -0600

    avanze del proyecto

---
+++ src/Components/Hero.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import './Hero.css'; 
+
+function Hero() {
+  return (
+    <div>
+   <div className="contenedor"> 
+     <div className="Texto">
+        <div className="box">
+            <samp>Bienvedido</samp> 
+            <h1>A Nuestra Landig Page</h1>
+            <p>Somos estudiantes de la carrera Infomática
+                Administrativa, en la cual cursamos la clase
+                llamada Lenguaje de la programación III. En la
+                cual esto es nuestro portafolio web.</p>   
+          </div>
+        </div>  
+   </div>
+   <div><a className="bn" href="#">Ver más</a></div>
+   </div>
+  );
+}
+
+export default Hero;
+
---
+++ src/Components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../Hero'
+import Footer from '../Footer'
+import Tarjeta from '../Tarjeta'
+
+function Home() {
+  return (
+    <>
+    <Hero />
+    <Tarjeta/>
+    <Footer />
+    </>
+  )
+}
+export default Home;
